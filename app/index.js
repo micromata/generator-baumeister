micromata/generator-baumeister

commit 7f16859e200e26eeebf677fd5ca018b71e93b4b8

feature(prompting): Define default for prompting a theme name
Remove validation for naming the theme because it’s unneeded when having a default value.

app/index.js
@@ -94,8 +94,7 @@ module.exports = class extends Generator {
 				{
 					type: 'input',
 					name: 'theme',
-					message: 'How should your Bootstrap theme be named in the Sass files?',
-					validate: helper.validateThemeName
+					default: 'theme'
 				},
 				{
 					type: 'list',

__tests__/test-app.js
@@ -992,15 +992,6 @@ describe('Baumeister using --yo-rc flag', () => {
 
 describe('Baumeister prompting helpers', () => {
 
-	describe('→ validateThemeName()', () => {
-		it('should accept any string', () => {
-			assert.equal(helper.validateThemeName('my-theme'), true);
-		});
-		it('should fail with an empty string', () => {
-			assert.equal(helper.validateThemeName(''), chalk.red('Oops. This is used to name a file and a directory and can’t left blank.'));
-		});
-	});
-
 	describe('→ validateSemverVersion()', () => {
 		it('should accept a valid semver version number', () => {
 			assert.equal(helper.validateSemverVersion('1.0.0'), true);

app/promptingHelpers.js
@@ -6,13 +6,6 @@ const error = chalk.red;
 
 const helper = {};
 
-helper.validateThemeName = function (value) {
-	if (value === '') {
-		return error('Oops. This is used to name a file and a directory and can’t left blank.');
-	}
-	return true;
-};
-
 helper.validateSemverVersion = function (value) {
 	if (semver.valid(value)) {
 		return true;

commit 482ecae0c1654ec704f73c8261ea6bdd33141b4f

fix(prompting): Fix broken prompt for theme name 🙈
Introduced with:

7f16859e feature(prompting): Define default for prompting a theme name

app/index.js
@@ -94,6 +94,7 @@ module.exports = class extends Generator {
 				{
 					type: 'input',
 					name: 'theme',
+					message: 'How should your Bootstrap theme be named in the Sass files?',
 					default: 'theme'
 				},
 				{