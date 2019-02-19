0734c5f01c68d915ce1192bdb1ceccf182bfa5f3
Michael Kühnel <m.kuehnel@micromata.de>
2019-02-19 20:50:20 +0100

    chore(generator): Reformat code using the latest settings

--- app/promptingHelpers.js
+++ app/promptingHelpers.js
@@ -7,23 +7,23 @@ const error = chalk.red;
 const helper = {};
 
 helper.validateSemverVersion = function(value) {
-	if (semver.valid(value)) {
-		return true;
-	}
+  if (semver.valid(value)) {
+    return true;
+  }
 
-	return error(
-		'Please enter a valid semver version, i.e. MAJOR.MINOR.PATCH. See → https://nodesource.com/blog/semver-a-primer/'
-	);
+  return error(
+    'Please enter a valid semver version, i.e. MAJOR.MINOR.PATCH. See → https://nodesource.com/blog/semver-a-primer/'
+  );
 };
 
 helper.defaultIssueTracker = function(answers) {
-	const regex = /(?:git@|https:\/\/)(github.com)(?::|\/{1})(.+).git/gi;
+  const regex = /(?:git@|https:\/\/)(github.com)(?::|\/{1})(.+).git/gi;
 
-	if (answers.projectRepository.match(regex) !== null) {
-		return answers.projectRepository.replace(regex, 'https://$1/$2/issues');
-	}
+  if (answers.projectRepository.match(regex) !== null) {
+    return answers.projectRepository.replace(regex, 'https://$1/$2/issues');
+  }
 
-	return '';
+  return '';
 };
 
 module.exports = helper;
