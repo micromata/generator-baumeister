d455382322517e63c6cf2389154fed17730bfb38
Michael Kühnel <m.kuehnel@micromata.de>
2019-02-20 10:30:49 +0100

    chore(baumeister): Reformat code using the latest settings

--- app/templates/build/webpack/_config.dev-server.js
+++ app/templates/build/webpack/_config.dev-server.js
@@ -5,16 +5,16 @@ import { isDevMode } from './helpers';
 import { stats } from './config.stats';
 
 export const devServer = {
-	contentBase: isDevMode()
-		? path.join(__dirname, '../', mainDirectories.dev)
-		: path.join(__dirname, '../', mainDirectories.prod),
-	port: isDevMode() ? 3000 : 3001,
-	overlay: true,
-	historyApiFallback: <%= templateProps.projectType == 'spa' ? 'true' : 'false' %>,
-	stats: {
-		version: false,
-		assets: false,
-		modules: false,
-		...stats
-	}
+  contentBase: isDevMode()
+    ? path.join(__dirname, '../', mainDirectories.dev)
+    : path.join(__dirname, '../', mainDirectories.prod),
+  port: isDevMode() ? 3000 : 3001,
+  overlay: true,
+  historyApiFallback: <%= templateProps.projectType == 'spa' ? 'true' : 'false' %>,
+  stats: {
+    version: false,
+    assets: false,
+    modules: false,
+    ...stats
+  }
 };
